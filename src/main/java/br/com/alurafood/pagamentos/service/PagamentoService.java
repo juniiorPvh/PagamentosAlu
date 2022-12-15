d8d6c4b2cb2b98dd16c822508382a4e216c94169
Junior Santos <juniior.pvh@gmail.com>
2022-12-15 09:46:47 -0400

    Adicionando método para excluir o pagamento no Service do projeto

--- src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -46,4 +46,8 @@ public class PagamentoService {
         pagamento = repository.save(pagamento);
         return modelMapper.map(pagamento, PagamentoDto.class);
     }
+
+    public void excluirPagamento(Long id){
+        repository.deleteById(id);
+    }
 }
